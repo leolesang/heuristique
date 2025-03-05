b234a5ba03dc0c893adad3a7a87dee105037ba3e
leova <leovallee02@gmail.com>
2025-03-05 19:01:12 +0100

    Ajout 1er fonctionnalite [LECTURE FICHIER TXT]

---
+++ main.c
@@ -0,0 +1,184 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int n, m;
+int *tab_coef;
+int *tab_m;
+int **tab_m_poid;
+
+void free_tab()
+{
+    free(tab_coef);
+    free(tab_m);
+
+    if (tab_m_poid)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            free(tab_m_poid[i]);
+        }
+        free(tab_m_poid);
+    }
+}
+
+void malloc_tab_m_poid()
+{
+    tab_m_poid = malloc(m * sizeof(int *));
+    if (!tab_m_poid)
+    {
+        printf("Erreur d'allocation mémoire pour tab_m_poid\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        tab_m_poid[i] = malloc(n * sizeof(int));
+        if (!tab_m_poid[i])
+        {
+            printf("Erreur d'allocation mémoire ligne %d\n", i);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void trim_spaces(char *str)
+{
+    int i = 0, j = 0;
+    int in_space = 0;
+
+    while (str[i])
+    {
+        if (!isspace((unsigned char)str[i]) || (j > 0 && !in_space))
+        {
+            str[j++] = str[i];
+        }
+        in_space = isspace((unsigned char)str[i]);
+        i++;
+    }
+    str[j] = '\0';
+}
+
+void insert_tab(char *myString, int count, int *tab)
+{
+    char *ptr = myString;
+    for (int i = 0; i < count; i++)
+    {
+        sscanf(ptr, "%d", &tab[i]);
+        while (*ptr && *ptr != ' ')
+            ptr++;
+        while (*ptr == ' ')
+            ptr++;
+    }
+}
+
+void read_file(char *file)
+{
+    FILE *fptr = fopen(file, "r");
+    if (!fptr)
+    {
+        printf("Impossible d'ouvrir le fichier.\n");
+        return;
+    }
+
+    char myString[256];
+    int ligne = 0;
+
+    // Lire la première ligne (n et m)
+    if (fgets(myString, sizeof(myString), fptr))
+    {
+        trim_spaces(myString);
+        sscanf(myString, "%d %d", &n, &m);
+
+        // Allocation memoire pour tab_coef, tab_m, et tab_m_poid
+        tab_coef = malloc(n * sizeof(int));
+        tab_m = malloc(m * sizeof(int));
+        malloc_tab_m_poid();
+
+        if (!tab_coef || !tab_m)
+        {
+            printf("Erreur d'allocation mémoire\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Lire les poids des objets (n valeurs)
+    int poids_index = 0;
+    while (poids_index < n && fgets(myString, sizeof(myString), fptr))
+    {
+        trim_spaces(myString);
+        int count = (poids_index + 10 <= n) ? 10 : (n - poids_index);
+        insert_tab(myString, count, tab_coef + poids_index);
+        poids_index += count;
+    }
+    fgets(myString, sizeof(myString), fptr);
+    //  Lire les capacites des contraintes (m valeurs)
+    int m_index = 0;
+    while (m_index < m && fgets(myString, sizeof(myString), fptr))
+    {
+        trim_spaces(myString);
+        int count = (m_index + 10 <= m) ? 10 : (m - m_index);
+        insert_tab(myString, count, tab_m + m_index);
+        m_index += count;
+    }
+    fgets(myString, sizeof(myString), fptr);
+
+    // Lire les poids des objets pour chaque contrainte (m * n valeurs)
+    for (int i = 0; i < m; i++)
+    {
+        int obj_index = 0;
+        while (obj_index < n && fgets(myString, sizeof(myString), fptr))
+        {
+            trim_spaces(myString);
+            // regarder si ligne vide ou non
+            if (strlen(myString) != 0)
+            {
+                int count = (obj_index + 10 <= n) ? 10 : (n - obj_index);
+                insert_tab(myString, count, tab_m_poid[i] + obj_index);
+                obj_index += count;
+            }
+        }
+    }
+
+    fclose(fptr);
+}
+
+void print_data()
+{
+    printf("Nombre d objets: %d, Nombre de contraintes: %d\n", n, m);
+
+    printf("\nCoefficients des objets :\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", tab_coef[i]);
+    }
+    printf("\n");
+
+    printf("\nCapacites des contraintes :\n");
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d ", tab_m[i]);
+    }
+    printf("\n");
+
+    printf("\nPoids des objets par contrainte :\n");
+    for (int i = 0; i < m; i++)
+    {
+        printf("Contrainte %d: ", i + 1);
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", tab_m_poid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    read_file("Instances_MKP/100M5_1.txt");
+    //  read_file("Instances_MKP/500M30_11.txt");
+    print_data();
+    free_tab();
+    return 0;
+}
